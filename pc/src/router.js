4737634e16e13d57c04b23276ed1b447672943ca
张果 <237625092@qq.com>
2018-06-27 10:17:25 +0800

    logo

--- pc/src/router.js
+++ pc/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './views/Login.vue'
 import Page from './views/Page.vue'
 import nav from './nav.js'
 
@@ -30,7 +30,7 @@ export default new Router({
     {
       path: '/',
       name: 'login',
-      component: Home
+      component: Login
     },
     {
       path: '/page',
